aa314b64c9c99aaab636d861b15f4ee15825cd38
John F Hutchinson <jfhutchi@us.ibm.com>
2014-10-29 16:26:31 -0400

    Update

--- lib/chef/knife/hmc_disk_add.rb
+++ lib/chef/knife/hmc_disk_add.rb
@@ -41,55 +41,69 @@ class Chef
 
       option :size,
         :short => "-S SIZE",
-        :long => "--pvid SIZE",
+        :long => "--size_in_GB SIZE",
         :description => "The size in GB you require, will find the size requested or larger."
 
       option :volume_group,
-        :short => "-v NAME",
+        :short => "-g NAME",
         :long => "--volume_group NAME",
         :description => "Name of volume group disk will be used in. If rootvg is passed then script will find a single LUN vs multiple smaller luns to fit request."
 
       def run
    		  Chef::Log.debug("Adding disk...")
 
-        validate!([:frame_name,:lpar_name,:vio2_name,:vio1_name,:size,:volume_group])
+        validate!([:frame_name,:lpar_name,:vio2_name,:vio1_name,:size])
 
-         hmc = Hmc.new(get_config(:hmc_host), get_config(:hmc_username) , {:password => get_config(:hmc_password)}) 
-         hmc.connect
+        hmc = Hmc.new(get_config(:hmc_host), get_config(:hmc_username) , {:password => get_config(:hmc_password)}) 
+        hmc.connect
 
-         #Populate hash to make LPAR object
-         lpar_hash = hmc.get_lpar_options(get_config(:frame_name),get_config(:lpar_name))
-         #Create LPAR object based on hash, and VIO objects
-         lpar = Lpar.new(lpar_hash)
-         vio1 = Vio.new(hmc, get_config(:frame_name), get_config(:vio1_name))
-         vio2 = Vio.new(hmc, get_config(:frame_name), get_config(:vio2_name))
+        #Populate hash to make LPAR object
+        lpar_hash = hmc.get_lpar_options(get_config(:frame_name),get_config(:lpar_name))
+        #Create LPAR object based on hash, and VIO objects
+        lpar = Lpar.new(lpar_hash)
+        vio1 = Vio.new(hmc, get_config(:frame_name), get_config(:vio1_name))
+        vio2 = Vio.new(hmc, get_config(:frame_name), get_config(:vio2_name))
 
-         #Get vSCSI Information
-          lpar_vscsi = lpar.get_vscsi_adapters
-          if lpar_vscsi.empty? == true
-            #Add vSCSI Adapters
+        #Get vSCSI Information
+        lpar_vscsi = lpar.get_vscsi_adapters
+        first_vhost = nil
+        second_vhost = nil
+        adapter_cnt = 0
+        if lpar_vscsi.empty? == true
+          #Add vSCSI Adapters
+          lpar.add_vscsi(vio1)
+          lpar.add_vscsi(vio2)
+        else
+          lpar_vscsi.each do |adapter|
+            if adapter.remote_lpar_name == vio1.Name
+              first_vhost = adapter.remote_slot_num
+              adapter_cnt += 1
+            elsif adapter.remote_lpar_name == vio2.Name
+              second_vhost = adapter.remote_slot_num
+              adapter_cnt += 1
+            end
+          end 
+          if first_vhost.nil? or second_vhost.nil? or adapter_cnt != 2
+            #vSCSIs not linked to VIOs specified adding new ones
             lpar.add_vscsi(vio1)
             lpar.add_vscsi(vio2)
-
-          else
-            #Iterate over the vSCSIs and test the names of the remote LPAR to validate that they are mapped to the VIOS specified
-            #If they are not then add 
             lpar_vscsi = lpar.get_vscsi_adapters
             #Find the vHosts
             first_vhost = vio1.find_vhost_given_virtual_slot(lpar_vscsi[0].remote_slot_num)
             second_vhost = vio2.find_vhost_given_virtual_slot(lpar_vscsi[1].remote_slot_num)
-
-
-           vio1 = Vio.new(hmc, get_config(:frame_name), get_config(:vio1_name))
-           vio2 = Vio.new(hmc, get_config(:frame_name), get_config(:vio2_name))
-           if get_config(:volume_group).downcase == "rootvg"
-             vio1.map_single_disk_by_size        
-            end
           end
-
+          if validate([:volume_group])
+            if get_config(:volume_group).to_s.downcase == "rootvg"
+              vio1.map_single_disk_by_size(first_vhost,vio2,second_vhost,get_config(:size).to_i)        
+            else
+              vio1.map_by_size(first_vhost,vio2,second_vhost,get_config(:size).to_i)
+            end
+          else
+            vio1.map_by_size(first_vhost,vio2,second_vhost,get_config(:size).to_i)
+          end
+        end
         hmc.disconnect        
       end
-
     end
   end
 end
